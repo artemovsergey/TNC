7acb630eca2053abba252eec7b391f5516cf874f
artik3314@gmail.com <artik3314@gmail.com>
2023-02-09 10:21:03 +0300

    Форма авторизации

---
+++ TNC.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TNC.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TNC.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TNC.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
