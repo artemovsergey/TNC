05c28fd1d900ad5bf3ad2e6cf8d15766718a8c6e
artik3314@gmail.com <artik3314@gmail.com>
2023-02-27 13:34:33 +0300

    update

---
+++ TaskApp/ViewModels/MainViiewModel.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApp.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string test;
+
+
+        [RelayCommand]
+        void SayText()
+        {
+            Test = "text1";
+        }
+
+
+        public MainViewModel()
+        {
+            Test = "test";
+        }
+
+    }
+}
